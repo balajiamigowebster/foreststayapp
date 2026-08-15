5392129b842ddbb6b3bbbb7bc8f4faabcc518b1f
balajiamigo <1988.balaji@gmail.com>
2026-08-15 20:31:21 +0530

    Update server.js with custom CORS preflight options handler to bypass Apache/cPanel preflight blocks

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,16 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
